9240f483a64b7b80e0991053384a681a1d907ca9
hilbug <65197724+hilbug@users.noreply.github.com>
2020-10-09 13:08:26 -0400

    seeds update db path

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let db = require("../models/index");
 
 mongoose.connect("mongodb://localhost/workouttrackerdb", {
   useNewUrlParser: true,

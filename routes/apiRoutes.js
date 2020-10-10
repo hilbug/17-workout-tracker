2fe55941acc4ca1be8ada111db5932a35fa80ddb
hilbug <65197724+hilbug@users.noreply.github.com>
2020-10-10 15:11:16 -0400

    apiRoutes - get all workout data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,17 +4,14 @@ const db = require("../models");
 
 /*
 Routes from public/api.js are:
-    // 1 getLastWorkout()
-    /api/workouts - get (fecth?)
-    
-    // 2 createWorkout()
-    /api/workouts - post(post with bulk?) (post?)
-    
-    // 3 addExercise()
-    /api/workouts/" + id - post (put?)
+    // 1 getLastWorkout(): /api/workouts - get
 
-    // getWorkoutsInRange()
-    /api/workouts/range get (fetch?)
+    // 2 getWorkoutsInRange(): /api/workouts/range - get
+    // from stats.js: get all workout data from back-end
+    
+    // 3 createWorkout(): /api/workouts - post
+    
+    // 4 addExercise(): /api/workouts/" + id - put
 */
 
 // 1 - get last work out
@@ -30,11 +27,22 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-// 2 - create workout
+// 2 - get all workout data from back-end
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+      console.log(err);
+    });
+});
+
+// 3 - create workout
 // when new workout is clicked, this is adding an entry to the db with no exercises
 // the url at this point is http://localhost:3000/?id=<the id of the creatd workout>
 router.post("/api/workouts", ({ body }, res) => {
-  console.log(body);
   db.Workout.create(body)
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -45,31 +53,29 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-// 3 - add exercise to the workout
+// 4 - add exercise to the workout
 // need to figure out how to add the exercise to the workout ID that was created above
 router.put("/api/workouts/:id", (req, res) => {
   const { id } = req.params;
 
   // find the document based on req.params.id
-  db.Workout.findOne(
-    { _id: id }
-  )
-  // then we get the document which is dbWorkout
-  .then(dbWorkout => {
-    // dbWorkout has an array of objects called "exercises" to which we push req.body
-    dbWorkout.exercises.push(req.body);
-    // then save the changes made to dbWorkout
-    return dbWorkout.save();
-  })
-  //send back result which is the updated dbWorkout to front-end
-  .then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  //catch and log errors if there are any
-  .catch(err => {
-    res.status(400).json(err);
-    console.log(err);
-  });
+  db.Workout.findOne({ _id: id })
+    // then we get the document which is dbWorkout
+    .then(dbWorkout => {
+      // dbWorkout has an array of objects called "exercises" to which we push req.body
+      dbWorkout.exercises.push(req.body);
+      // then save the changes made to dbWorkout
+      return dbWorkout.save();
+    })
+    //send back result which is the updated dbWorkout to front-end
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    //catch and log errors if there are any
+    .catch(err => {
+      res.status(400).json(err);
+      console.log(err);
+    });
 });
 
 module.exports = router;

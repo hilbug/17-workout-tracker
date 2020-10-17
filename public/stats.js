e51430d41e7ce1bd76c42d1caf9d24cf977e827c
hilbug <65197724+hilbug@users.noreply.github.com>
2020-10-17 09:50:51 -0400

    update stats so charts are accurate

--- public/stats.js
+++ public/stats.js
@@ -6,13 +6,17 @@ fetch("/api/workouts/range")
   })
   .then(data => {
     populateChart(data);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+    console.log(err);
   });
 
 
 API.getWorkoutsInRange()
 
-  function generatePalette() {
-    const arr = [
+function generatePalette() {
+  const arr = [
     "#003f5c",
     "#2f4b7c",
     "#665191",
@@ -32,9 +36,19 @@ API.getWorkoutsInRange()
   ]
 
   return arr;
-  }
+}
+
 function populateChart(data) {
   let durations = duration(data);
+
+  let totalDurations = durationMap(data);
+  let totalDurationsDate = totalDurations.map(element => element.date);
+  let totalDurationsDuration = totalDurations.map(element => element.totalDuration);
+
+  let totalWeights = weightMap(data);
+  let totalWeightsDate = totalWeights.map(element => element.date);
+  let totalWeightsWeight = totalWeights.map(element => element.totalWeight);
+
   let pounds = calculateTotalWeight(data);
   let workouts = workoutNames(data);
   const colors = generatePalette();
@@ -47,21 +61,13 @@ function populateChart(data) {
   let lineChart = new Chart(line, {
     type: "line",
     data: {
-      labels: [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-      ],
+      labels: totalDurationsDate,
       datasets: [
         {
           label: "Workout Duration In Minutes",
           backgroundColor: "red",
           borderColor: "red",
-          data: durations,
+          data: totalDurationsDuration,
           fill: false
         }
       ]
@@ -95,19 +101,11 @@ function populateChart(data) {
   let barChart = new Chart(bar, {
     type: "bar",
     data: {
-      labels: [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ],
+      labels: totalWeightsDate,
       datasets: [
         {
           label: "Pounds",
-          data: pounds,
+          data: totalWeightsWeight,
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
@@ -186,6 +184,17 @@ function populateChart(data) {
   });
 }
 
+function formatDate(date) {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
+  return new Date(date).toLocaleDateString(options);
+}
+
 function duration(data) {
   let durations = [];
 
@@ -197,6 +206,39 @@ function duration(data) {
 
   return durations;
 }
+
+function durationMap(data) {
+  let durationsMap = [];
+
+  data.forEach(workout => {
+    durationsMap.push(
+      {
+        date: formatDate(workout.day),
+        totalDuration: workout.totalDuration,
+        totaldurationtype: typeof workout.totalDuration
+      }
+    );
+  });
+ 
+  return durationsMap.slice(Math.max(durationsMap.length - 10, 0));
+}
+
+function weightMap(data) {
+  let weightsMap = [];
+
+  data.forEach(workout => {
+    weightsMap.push(
+      {
+        date: formatDate(workout.day),
+        totalWeight: workout.totalWeight,
+        weighttype: typeof workout.totalWeight
+      }
+    );
+  });
+  
+  return weightsMap.slice(Math.max(weightsMap.length - 10, 0));
+}
+
 
 function calculateTotalWeight(data) {
   let total = [];
@@ -218,6 +260,6 @@ function workoutNames(data) {
       workouts.push(exercise.name);
     });
   });
-  
+
   return workouts;
 }
